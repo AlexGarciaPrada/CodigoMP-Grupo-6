6cdbfa66bf4f7573b31824c89c776026de6b6240
lauraxpb <l.pineda.2022@alumnos.urjc.es>
2024-03-12 12:47:03 +0100

    saveUser y loadUser

--- Combate2000LaSecuela/src/combate2000lasecuela/Usermanager.java
+++ Combate2000LaSecuela/src/combate2000lasecuela/Usermanager.java
@@ -1,6 +1,12 @@
 package combate2000lasecuela;
 
 import java.util.HashMap;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class Usermanager {
     private HashMap<String, User> users;
@@ -17,7 +23,30 @@ public class Usermanager {
         return users.remove(nick);
     }
 
-    public User saveUser(User user) {
+    public static void saveUser(User user) {
+            try {
+                XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("./config/users/" + user.getNick() + ".xml")));
+                encoder.writeObject(user);  //writeObject recibe un Object, por eso no hay que hacer el casting
+                encoder.close();
+            }
+            catch (FileNotFoundException fileNotFoundException) {
+                System.out.println("ERROR SAVING USER");
+            }
+        }
 
+    public static User loadUsers(String usersFileName) {
+        try {
+            XMLDecoder decoder = new XMLDecoder(new FileInputStream("./config/users/" + usersFileName + ".xml"));    // Abre el archivo para lectura.
+            User user = (User) decoder.readObject();  //readObject devuelve un Object, por eso hay que hacer el casting
+            decoder.close();
+            return user;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("ERROR LOADING GAME");
+            e.printStackTrace();
+            return null;
+        }
     }
 }
+
+
