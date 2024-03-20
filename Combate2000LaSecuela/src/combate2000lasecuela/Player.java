7870c43b5e6420c386f749edd0c8c0314296ffbb
Ikram <ikrameljauhari@gmail.com>
2024-03-20 13:11:46 +0100

    haciendo player

--- Combate2000LaSecuela/src/combate2000lasecuela/Player.java
+++ Combate2000LaSecuela/src/combate2000lasecuela/Player.java
@@ -3,6 +3,7 @@ package combate2000lasecuela;
 import java.util.Map;
 import java.util.UUID;
 
+import combate2000lasecuela.CosasDeLuchador.Fighter;
 import combate2000lasecuela.CosasDeLuchador.TFighter;
 import combate2000lasecuela.managers.ChallengeManager;
 public class Player extends User {
@@ -10,34 +11,37 @@ public class Player extends User {
     private int victories;
     private boolean blocked;
 
+    private Fighter fighter;
+
     public Player(String name, String password, String nick) {
         super(name, password, nick);
         this.registerNumber = registerNumber;
         this.victories = victories;
         this.blocked = blocked;
+        this.fighter = fighter;
     }
 
-    public static TFighter elegirTipo() {
-        return null;
-    }
+    public int getVictories() {return victories;}
 
-    public Challenge ChallengePlayer(){
-        String challenged = getMessageManager().challengeMenu();
-        if (isAPlayer(challenged)) {
-            int amount = getMessageManager().selectGold();
-        }
+    public Fighter getFighter() {return fighter;}
+
+    public void setBlocked() {blocked = true;}
+
+    public boolean isBlocked() {return blocked;}
+
+    public void setFighter(Fighter character) {fighter = character;}
+
+    public void deleteFighter() {fighter = null;}
+
+    public Challenge ChallengePlayer() {
         ChallengeManager cm = new ChallengeManager();
         cm.addElement("Challenge", generateRandomChallengeKey(), new Challenge());
         return cm.loadElement(generateRandomChallengeKey());
     }
 
-    public boolean isAPlayer(String playerNick) {return true;}
-    public void createCharacter() {
-        
-    }
     public static String generateRandomChallengeKey() {
         return UUID.randomUUID().toString();
     }
 
-
+    public void fight(Challenge c) {}
 }
