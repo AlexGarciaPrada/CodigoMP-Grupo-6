66bc863342015dd5191c9ddcf3211c6361661610
Ikram <ikrameljauhari@gmail.com>
2024-03-23 14:16:36 +0100

    cambios generales

--- Combate2000LaSecuela/src/combate2000lasecuela/Player.java
+++ Combate2000LaSecuela/src/combate2000lasecuela/Player.java
@@ -13,6 +13,10 @@ public class Player extends User {
 
     private Fighter fighter;
 
+    private boolean deleted;
+
+
+
     public Player(String name, String password, String nick) {
         super(name, password, nick);
         this.registerNumber = registerNumber;
@@ -24,6 +28,7 @@ public class Player extends User {
         this.victories=0;
         this.blocked = false;
         this.fighter =null;
+        this.deleted = false;
 
     }
 
@@ -39,11 +44,24 @@ public class Player extends User {
 
     public boolean isBlocked() {return blocked;}
 
+
+    //estos dos para "darse de baja" (querer borrar la cuenta), en gameFlow se borrará el usuario del mapa
+    public void setDeleted() {deleted = true;}
+
+    public boolean isDeleted() {return deleted;}
+
+
+    //Tendria que ser algo asi creo si Dani considera modificar el constructor de fighter
+
+    //public void registerFighter(String name, int type) {
+    //Fighter f = new Fighter(name, type);
+    //setFighter(f);}
+
     public void setFighter(Fighter character) {fighter = character;}
 
     public void deleteFighter() {fighter = null;}
 
-    public Challenge ChallengePlayer() {
+    public Challenge ChallengePlayer(Player challenged) {
         ChallengeManager cm = new ChallengeManager();
         cm.addElement("Challenge", generateRandomChallengeKey(), new Challenge());
         return cm.loadElement(generateRandomChallengeKey());
@@ -53,5 +71,9 @@ public class Player extends User {
         return UUID.randomUUID().toString();
     }
 
-    public void fight(Challenge c) {}
+    //sigo pensando en si tiene sentido porque pendingChallenges tiene relacion con fighter
+
+   // public void fight(Challenge c) {
+    //    fighter.startFighting(c);
+    //    }
 }
