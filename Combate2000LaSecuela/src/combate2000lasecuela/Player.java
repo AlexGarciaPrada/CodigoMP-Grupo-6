893ae282d773d719f90562888b3a76abc4188e68
Ikram <ikrameljauhari@gmail.com>
2024-03-24 11:04:21 +0100

    Update Player.java

--- Combate2000LaSecuela/src/combate2000lasecuela/Player.java
+++ Combate2000LaSecuela/src/combate2000lasecuela/Player.java
@@ -13,22 +13,15 @@ public class Player extends User {
 
     private Fighter fighter;
 
-    private boolean deleted;
-
 
 
     public Player(String name, String password, String nick) {
         super(name, password, nick);
         this.registerNumber = registerNumber;
-
-        this.victories = victories;
-        this.blocked = blocked;
-        this.fighter = fighter;
-
         this.victories=0;
         this.blocked = false;
         this.fighter =null;
-        this.deleted = false;
+
 
     }
 
@@ -36,28 +29,16 @@ public class Player extends User {
 
     public Fighter getFighter() {return fighter;}
 
-
-    public void setBlocked() {blocked = true;}
-
     public void setBlocked(boolean state) {blocked = state;}
 
 
     public boolean isBlocked() {return blocked;}
 
 
-    //estos dos para "darse de baja" (querer borrar la cuenta), en gameFlow se borrará el usuario del mapa
-    public void setDeleted() {deleted = true;}
-
-    public boolean isDeleted() {return deleted;}
+    //public void registerFighter(Fighter f) {
+    //this.fighter = f;}
 
 
-    //Tendria que ser algo asi creo si Dani considera modificar el constructor de fighter
-
-    //public void registerFighter(String name, int type) {
-    //Fighter f = new Fighter(name, type);
-    //setFighter(f);}
-
-    public void setFighter(Fighter character) {fighter = character;}
 
     public void deleteFighter() {fighter = null;}
 
