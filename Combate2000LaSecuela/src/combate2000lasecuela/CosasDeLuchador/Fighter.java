71b61c7058b6be30847bcded51cbb0e02bd4749f
fuihfuefuiewn <131182551+fuihfuefuiewn@users.noreply.github.com>
2024-03-24 10:43:47 +0100

    Preparativos
    Como estuvimos hablando ayer delego en el DataBase lo de hacer las pilas.

--- Combate2000LaSecuela/src/combate2000lasecuela/CosasDeLuchador/Fighter.java
+++ Combate2000LaSecuela/src/combate2000lasecuela/CosasDeLuchador/Fighter.java
@@ -22,57 +22,41 @@ public abstract class Fighter {
         private int suerteM;
         private int suerteW;
         private int suerteA;
-        private Fighter type;
+        private String clase;
+        private TFighter type;
         private MinionManager minionManager;
-        public Fighter(int suerteA,int suerteW,int suerteM) {
+        private ItemManager itemManager;
+        public Fighter(String name, TFighter type,String clase,
+        Stack<Minion> myMinions,Stack<Armor> myArmor,Stack<Weapon> myWeapon) {
             this.name = name;
             this.health = health;
             this.power = power;
-            this.type = this;
+            this.clase=clase;
+            this.type = type;
             this.suerteM=type.suerteM;
             this.suerteW=type.suerteW;
             this.suerteA=type.suerteA;
-            this.myMinions = randomMinions(suerteM);
-            this.myArmor= randomArmor(suerteA);
-            this.myWeapon = randomWeapons(suerteW);
+            this.myMinions = myMinions;
+            this.myArmor= myArmor;
+            this.myWeapon = myWeapon;
         }
 
-        private Stack<Minion> randomMinions(int suerte){
-            Random random = new Random();
-            Minion esclavo;
-            int numero= random.nextInt(80)+1+suerte;
-            for (Integer i=0; i<=numero;i++){
-                String aux = i.toString();
-                esclavo = minionManager.getElements().get("MinionMap").get(aux);
-                if (!("Vampire".equals(this.type)) || !(esclavo instanceof Human)){
-                    this.myMinions.push(esclavo);
-                }
-            }
-            return this.myMinions;
-        } //mandar todo esto a DataBase
-    private Stack<Weapon> randomWeapons(int suerte) {
-        Random random = new Random();   
-        Weapon arma;
-        int numero = random.nextInt(28) + 1 + suerte;
-        for (int i=1; i<=numero;i++){
-            arma = ItemManager.weaponMap.get(numero);
-            this.myWeapon.push(arma);
-        }
-        return this.myWeapon;
-    }
-    private Stack<Armor> randomArmor(int suerte){
-        Random random = new Random();
-        Armor armadura;
-        int numero= random.nextInt(28)+1+suerte;
-        for (int i=1; i<=numero; i++) {
-            armadura = ItemManager.armorMap.get(numero);
-            this.myArmor.push(armadura);
-        }
-        return this.myArmor;
-    }
-    public Combat startFighting(){
+
+    public Combat startFighting(Fighter desafiante){
             Textterminal terminal = new Textterminal();
-            terminal.show ("Quieres empezar un combate");
+            int i=0;
+            do {
+                i++;
+                terminal.show("Ronda numero" + i + "comienza");
+                int pA=potencialAtaque();
+                int pD= potencialDefensa();
+            }while((this.health>0)||(desafiante.health>0));
             return null;
+    }
+    private int potencialAtaque (){
+            return 0;
+    }
+    private int potencialDefensa (){
+            return 0;
     }
 }
