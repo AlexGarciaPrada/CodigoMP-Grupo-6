7cdafeabc6664d6cb31a9e077a02ce2a4de802ef
fuihfuefuiewn <131182551+fuihfuefuiewn@users.noreply.github.com>
2024-03-26 11:04:07 +0100

    Squashed commit of the following:
    commit ab925d3ba5d501da34dd7ac572e5c653d26287f7
    Author: fuihfuefuiewn <131182551+fuihfuefuiewn@users.noreply.github.com>
    Date:   Tue Mar 26 10:51:33 2024 +0100
    
        Ya veo lo que dice Alex
    
        Alex tienes razón, hay algo que no termina de funcionar o no está hecho
    
    commit 0d41ab4757174569c580fbd6a45c26a7bbfe84b8
    Author: fuihfuefuiewn <131182551+fuihfuefuiewn@users.noreply.github.com>
    Date:   Tue Mar 26 10:46:12 2024 +0100
    
        GameFlow
    
        Actualización necesaria
    
    commit ec11ce1baf5d907a0e1426beade6cdd012fd3a9e
    Merge: 15ca2b4 c4a40ad
    Author: fuihfuefuiewn <131182551+fuihfuefuiewn@users.noreply.github.com>
    Date:   Tue Mar 26 10:31:39 2024 +0100
    
        Merge branch 'main' into fuihfuefuiewnBranch
    
    commit 15ca2b44213c6eca6cbb9167915aca29f13f18ed
    Author: fuihfuefuiewn <131182551+fuihfuefuiewn@users.noreply.github.com>
    Date:   Tue Mar 26 10:31:04 2024 +0100
    
        Primer paso

--- Combate2000LaSecuela/src/combate2000lasecuela/Player.java
+++ Combate2000LaSecuela/src/combate2000lasecuela/Player.java
@@ -24,6 +24,36 @@ public class Player extends User {
         this.blocked = false;
         this.fighter =null;
     }
+    public void Fight(Player playerDesafiado, int oroApostado){
+        int opcion=0;
+        if (haveFighter(this)&&(haveFighter(playerDesafiado))) {
+            Fighter desafiante = this.getFighter();
+            Fighter desafiado = playerDesafiado.getFighter();
+            if (desafiante.getArmadura()==null) {
+                getNumero(opcion);
+                changeActiveArmor(this,desafiante.getMyArmor(),opcion);
+            }else if (desafiante.getArma1()==null){
+                changeActiveWeapon(this,desafiante.getMyWeapon(),getArma(opcion));
+            }
+
+            if (desafiado.getArmadura()==null) {
+                getNumero(opcion);
+                changeActiveArmor(playerDesafiado,desafiado.getMyArmor(),opcion);
+            }else if (desafiado.getArma1()==null){
+                changeActiveWeapon(playerDesafiado,desafiado.getMyWeapon(),getArma(opcion));
+            }
+            //comprobaciones que petarían el combater terminadas
+            Combat combate= desafiado.startFighting(desafiante,oroApostado);
+            //falta guardar en el registro de combates el resultado
+        }
+    }
+    public String getArma(int opcion){
+        return Integer.toString(opcion);
+    }
+    public int getNumero(int opcion){
+        //desde el GameFLow tengo que recibir el scanner de la opcion
+        return opcion;
+    }
 
     public int getVictories() {return victories;}
 
@@ -51,7 +81,9 @@ public class Player extends User {
     }
 
     public void deleteFighter() {fighter = null;}
-
+    public boolean haveFighter(Player p){
+        return (p.fighter!=null);
+    }
 
     public Challenge challengePlayer(Player challenged, int gold) {
         if (this.fighter != null && challenged.getFighter() != null) {
@@ -70,10 +102,6 @@ public class Player extends User {
     }
     public void rejectingChallenge(int gold){
         this.getFighter().setGold(this.getFighter().getGold()- (int) (gold*0.1));
-    }
-
-    public void fight(Player challenged) {
-        fighter.startFighting(challenged.getFighter());
     }
 
     public void changeActiveWeapon(Player player, LinkedList<Weapon> MyWeapons, String weapon) {
