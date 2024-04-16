db02b8ca881820d77b28dc554b8a64912bce986e
lauraxpb <l.pineda.2022@alumnos.urjc.es>
2024-04-17 00:52:29 +0200

    buenas noches

--- Combate2000LaSecuela/Tests/combate2000lasecuela/OperatorFlowTest.java
+++ Combate2000LaSecuela/Tests/combate2000lasecuela/OperatorFlowTest.java
@@ -4,7 +4,12 @@ import combate2000lasecuela.OperatorFlow;
 import combate2000lasecuela.Player;
 import combate2000lasecuela.managers.Database;
 import combate2000lasecuela.managers.UserManager;
+import combate2000lasecuela.screen.MessageManager;
 import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class OperatorFlowTest {
 
@@ -13,6 +18,8 @@ public class OperatorFlowTest {
         Database database = new Database();
         Player playerTester = new Player("pepe","00","pepe");
         database.addPlayer(playerTester);
+        Operator operatorTester = new Operator("op","00","op");
+        database.addOperator(operatorTester);
     }
 
     @Test
@@ -20,7 +27,13 @@ public class OperatorFlowTest {
     }
 
     @Test
-    void operatorLogin() {
+    void operatorLoginEditFighter() {
+        String data = "1";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        OperatorFlow operatorFlow = new OperatorFlow();
+        //MessageManager messageManager = new MessageManager();
+        //operatorFlow.set
+        assertEquals(Integer.parseInt(data)==1, operatorFlow.isEditfighter());
     }
 
     @Test
