18a1d46ae5a1197b7e46405ad04008cc301b6b53
fuihfuefuiewn <131182551+fuihfuefuiewn@users.noreply.github.com>
2024-04-20 14:34:41 +0200

    Mas cosas
    Más test que faltaban.

--- Combate2000LaSecuela/Tests/combate2000lasecuela/CosasDeLuchador/GhoulTest.java
+++ Combate2000LaSecuela/Tests/combate2000lasecuela/CosasDeLuchador/GhoulTest.java
@@ -1,7 +1,16 @@
 package combate2000lasecuela.CosasDeLuchador;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class GhoulTest {
 
-class GhoulTest {
+    @Test
+    public void testGetSpecialSkillName() {
+        // Crear un Ghoul con una línea de datos
+        String linea = "5; NEFARIUS; GHOUL; 3; 1;";
+        Ghoul ghoul = new Ghoul(linea);
 
+        // Verificar que el nombre de la habilidad especial sea "Dependencia"
+        assertEquals("Dependencia", ghoul.getSpecialSkillName());
+    }
 }
--- Combate2000LaSecuela/src/combate2000lasecuela/CosasDeLuchador/Demon.java
+++ Combate2000LaSecuela/src/combate2000lasecuela/CosasDeLuchador/Demon.java
@@ -6,8 +6,6 @@ import java.util.LinkedList;
 
 public class Demon extends Minion implements Serializable {
     private LinkedList<Minion> listaDemoniaca;
-//    private MinionAttributes addedAttribute = MinionAttributes.Pacto;
-
     public Demon(String linea) {
         super(linea);
         String [] valores = linea.split(";");
